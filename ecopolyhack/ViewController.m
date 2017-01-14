98d05e3ad6b488f06c01ab3b974c47972a678eeb
elliotfiske <elliotfiske@gmail.com>
2017-01-14 09:02:58 -0800

    Initial Commit

---
+++ ecopolyhack/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ecopolyhack
+//
+//  Created by Elliot Fiske on 1/14/17.
+//  Copyright © 2017 Elliot Fiske. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+   [super viewDidLoad];
+   // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+   [super didReceiveMemoryWarning];
+   // Dispose of any resources that can be recreated.
+}
+
+
+@end
