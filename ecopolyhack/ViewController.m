ff760667bca5d9078f8323e9cac5c96cd1c4523c
elliotfiske <elliotfiske@gmail.com>
2017-01-14 10:09:29 -0800

    Init commit

--- ecopolyhack/ViewController.m
+++ ecopolyhack/ViewController.m
@@ -10,20 +10,55 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+
 @end
 
 @implementation ViewController
 
+- (IBAction)touchDownFacebook:(UIView *)fbButton {
+   [UIView animateWithDuration:0.1
+                         delay:0
+                       options:UIViewAnimationOptionCurveEaseIn animations:^{
+                          fbButton.transform = CGAffineTransformMakeScale(1.15,1.15);
+                          fbButton.backgroundColor = [UIColor brownColor];
+                       } completion:nil];
+
+}
+
+- (IBAction)touchUpFacebook:(UIView *)fbButton {
+   [UIView animateWithDuration:0.18
+                         delay:0
+                       options:UIViewAnimationOptionCurveEaseIn animations:^{
+                          fbButton.transform = CGAffineTransformMakeScale(0.001,0.001);
+                          fbButton.backgroundColor = [UIColor brownColor];
+                       } completion:^(BOOL finished){
+                          
+                          
+                       }];
+   
+//   [UIView animateWithDuration: (CGFloat) 0.15 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:5 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+//      spinner.transform = CGAffineTransformMakeScale(2.0, 2.0)
+//   } completion:completion:^(BOOL finished){
+//      
+//      
+//   }];
+   
+}
+
+
 - (void)viewDidLoad {
    [super viewDidLoad];
    // Do any additional setup after loading the view, typically from a nib.
+   self.spinner.transform = CGAffineTransformMakeScale(0.001,0.001);
+   
 }
-
 
 - (void)didReceiveMemoryWarning {
    [super didReceiveMemoryWarning];
    // Dispose of any resources that can be recreated.
 }
+
 
 
 @end
