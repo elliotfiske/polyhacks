4aadd828fe472e1fae3efa133abb7df79773521f
elliotfiske <elliotfiske@gmail.com>
2017-01-14 19:03:13 -0800

    camera only takes videos

---
+++ ecopolyhack/PlayVideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayVideoViewController.h
+//  ecopolyhack
+//
+//  Created by Elliot Fiske on 1/14/17.
+//  Copyright © 2017 Elliot Fiske. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayVideoViewController : UIViewController
+
+@end
---
+++ ecopolyhack/PlayVideoViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlayVideoViewController.m
+//  ecopolyhack
+//
+//  Created by Elliot Fiske on 1/14/17.
+//  Copyright © 2017 Elliot Fiske. All rights reserved.
+//
+
+#import "PlayVideoViewController.h"
+
+@interface PlayVideoViewController ()
+
+@end
+
+@implementation PlayVideoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
